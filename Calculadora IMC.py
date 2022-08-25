97e61e007497f2d89652b564b4ab6431cb12d6f6
OLAF CRUZ <chucholaf@hotmail.com>
2022-08-25 10:03:04 -0500

    Proyecto módulo 1

---
+++ Calculadora IMC.py
@@ -0,0 +1,21 @@
+# Este programa calcula el Índice de masa corporal de una persona y arroja una leyenda dependiendo los datos de la persona.
+
+#En nombre y apellidos, hay que validar que sean solo letras.
+
+nombre = input("Escribe tu nombre: ")
+    # if nombre == "":
+        print("No escribiste tu nombre")
+
+# Capellido_paterno = input("Escribe tu apellido paterno: ")
+
+# apellido_materno = input("Escribe tu apellido materno: ")
+
+# edad = int(input("Teclea tu edad: "))
+
+# peso = float(input("Teclea tu peso en Kilos: "))
+
+# estatura = float(input("Teclea tu estatura en metros: "))
+
+# imc = peso / (estatura ** 2)
+
+# print(imc)
