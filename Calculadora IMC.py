07d7eec26d578117717f1c5f79693a7f4c27683b
OLAF CRUZ <chucholaf@hotmail.com>
2022-08-25 11:43:25 -0500

    Segundo avance Proyecto Módulo 1

--- Calculadora IMC.py
+++ Calculadora IMC.py
@@ -1,10 +1,21 @@
 # Este programa calcula el Índice de masa corporal de una persona y arroja una leyenda dependiendo los datos de la persona.
 
-#En nombre y apellidos, hay que validar que sean solo letras.
+# En nombre y apellidos, hay que validar que sean solo letras.
 
-nombre = input("Escribe tu nombre: ")
-    # if nombre == "":
-        print("No escribiste tu nombre")
+
+
+nombre = str(input("Escribe tu nombre: "))
+# Bucle While para no dejar una variable vacia.
+while nombre == "":
+    nombre = str(input("No escribiste nada, por favor Escribe tu nombre: "))
+
+
+        # if not nombre:
+        #     print("No escribiste")
+#     else:
+#         print(nombre)
+# print ("ok")
+
 
 # Capellido_paterno = input("Escribe tu apellido paterno: ")
 
@@ -18,4 +29,28 @@ nombre = input("Escribe tu nombre: ")
 
 # imc = peso / (estatura ** 2)
 
+
+# Impresión de datos usando el metodo "upper"
+print("Nombre: ", nombre.upper())
 # print(imc)
+
+
+
+# cadena = "1"
+# # Comprobar con la forma idiomática
+# if not cadena:
+# 	print("La cadena está vacía")
+# else:
+# 	print("La cadena NO está vacía")
+
+# # O contar sus caracteres
+# if len(cadena) is 0:
+# 	print("La cadena está vacía")
+# else:
+# 	print("La cadena NO está vacía")
+
+# # Lo contrario de hace un momento:
+# if cadena:
+# 	print("Ok la cadena no está vacía")
+# else:
+# 	print("La cadena está vacía")
